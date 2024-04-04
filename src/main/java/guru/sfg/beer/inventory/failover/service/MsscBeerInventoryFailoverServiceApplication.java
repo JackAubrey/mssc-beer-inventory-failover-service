1a010882bb2c17971d12f6426ae505a4e4edaf69
danilo <d.cividin@hotmail.it>
2024-04-04 18:15:12 +0200

    enabled context propagation in order to works with reactor

--- src/main/java/guru/sfg/beer/inventory/failover/service/MsscBeerInventoryFailoverServiceApplication.java
+++ src/main/java/guru/sfg/beer/inventory/failover/service/MsscBeerInventoryFailoverServiceApplication.java
@@ -3,12 +3,14 @@ package guru.sfg.beer.inventory.failover.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import reactor.core.publisher.Hooks;
 
 @ComponentScan(basePackages={"guru.sfg"})
 @SpringBootApplication
 public class MsscBeerInventoryFailoverServiceApplication {
 
 	public static void main(String[] args) {
+		Hooks.enableAutomaticContextPropagation();
 		SpringApplication.run(MsscBeerInventoryFailoverServiceApplication.class, args);
 	}
 
